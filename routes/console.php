8f2eac37fc40d96efb2eb57da6590f9728539122
Abd Whb <winnerk088@gmail.com>
2025-04-03 22:15:38 +0100

    Update

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command("queue:work --stop-when-empty")->everyFiveMinutes();
+Schedule::command('telescope:prune --hours=72')->daily();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
