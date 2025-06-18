957df408183b34f8ecc7d526d3001a11166c521b
Abd Whb <winnerk088@gmail.com>
2025-06-19 00:14:14 +0100

    Add missing casts for invert_code, secure_api, and delay in Setting model

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     protected $fillable = ['invert_code', 'secure_api', 'receiver_email', 'delay'];
+
+    protected $casts = [
+        'invert_code' => 'boolean',
+        'secure_api' => 'boolean',
+        'delay' => 'integer',
+    ];
 }
